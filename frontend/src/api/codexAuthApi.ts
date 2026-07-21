151f8b1c608eadce32e6ea4de64bee18690535a2
Kings Of The Web <kingsofthewebofficial@gmail.com>
2026-07-21 12:33:02 -0400

    refactor(frontend): unify device auth APIs

--- frontend/src/api/codexAuthApi.ts
+++ frontend/src/api/codexAuthApi.ts
@@ -1,16 +1,12 @@
-import { requestJson } from "./apiRequest";
-import { subscribeToJsonMessages } from "../transport/jsonMessageSubscription";
+import { DeviceAuthApi } from "./deviceAuthApi";
 import type { CodexAuthStatus, CodexDeviceLogin } from "../models/auth";
 import { API_ROUTES, WEB_SOCKET_ROUTES } from "../config/routes";
 
-export const codexAuthApi = {
-  status: () => requestJson<CodexAuthStatus>("GET", API_ROUTES.codexAuth.status),
-  startDeviceLogin: () =>
-    requestJson<CodexDeviceLogin>(
-      "POST",
-      API_ROUTES.codexAuth.startDeviceLogin,
-      {}
-    ),
-  subscribe: (onStatus: (status: CodexAuthStatus) => void) =>
-    subscribeToJsonMessages(WEB_SOCKET_ROUTES.codexAuthStatus, onStatus),
-};
+export const codexAuthApi = new DeviceAuthApi<
+  CodexAuthStatus,
+  CodexDeviceLogin
+>({
+  status: API_ROUTES.codexAuth.status,
+  startDeviceLogin: API_ROUTES.codexAuth.startDeviceLogin,
+  statusUpdates: WEB_SOCKET_ROUTES.codexAuthStatus,
+});
--- frontend/src/api/kimiAuthApi.ts
+++ frontend/src/api/kimiAuthApi.ts
@@ -1,16 +1,9 @@
-import { requestJson } from "./apiRequest";
-import { subscribeToJsonMessages } from "../transport/jsonMessageSubscription";
+import { DeviceAuthApi } from "./deviceAuthApi";
 import type { KimiAuthStatus, KimiDeviceLogin } from "../models/auth";
 import { API_ROUTES, WEB_SOCKET_ROUTES } from "../config/routes";
 
-export const kimiAuthApi = {
-  status: () => requestJson<KimiAuthStatus>("GET", API_ROUTES.kimiAuth.status),
-  startDeviceLogin: () =>
-    requestJson<KimiDeviceLogin>(
-      "POST",
-      API_ROUTES.kimiAuth.startDeviceLogin,
-      {}
-    ),
-  subscribe: (onStatus: (status: KimiAuthStatus) => void) =>
-    subscribeToJsonMessages(WEB_SOCKET_ROUTES.kimiAuthStatus, onStatus),
-};
+export const kimiAuthApi = new DeviceAuthApi<KimiAuthStatus, KimiDeviceLogin>({
+  status: API_ROUTES.kimiAuth.status,
+  startDeviceLogin: API_ROUTES.kimiAuth.startDeviceLogin,
+  statusUpdates: WEB_SOCKET_ROUTES.kimiAuthStatus,
+});
